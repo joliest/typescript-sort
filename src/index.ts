ed608046fa84a5fcedfdbd73a283a13f55619a33
Joliver Estampador <joliverestampador@gmail.com>
2022-08-02 08:22:53 +0800

    good solution (will put another refactor in a bit)

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,7 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
+import { NumbersCollection } from './NumbersCollection';
+import { Sorter } from './sorter';
 
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        // if collection is an array of number
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-
-        // if collection is string do this logic instead?
-        if (typeof this.collection === 'string') {
-          // this.collection will provide an autocomplete dropdown that
-          // contains the method of string
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
