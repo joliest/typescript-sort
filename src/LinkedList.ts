b47715f3bb13f4eb51051fcdb3beeaa1c7897212
Joliver Estampador <joliverestampador@gmail.com>
2022-08-03 08:14:33 +0800

    linked list implementation

---
+++ src/LinkedList.ts
@@ -0,0 +1,85 @@
+class Node {
+  next: Node | null = null;
+  constructor(public data: number) {}
+}
+
+export class LinkedList {
+  head: Node | null = null;
+  add(data: number): void {
+    const node = new Node(data);
+    if (!this.head) {
+      this.head = node;
+      return; // you can still use return keyword as long as you dont return anything
+    }
+
+    let tail = this.head;
+    while (tail.next) {
+      tail = tail.next;
+    }
+
+    tail.next = node;
+  }
+
+  get length(): number {
+    if (!this.head) return 0;
+
+    let length = 1;
+    let node = this.head;
+    while (node.next) {
+      length++;
+      node = node.next;
+    }
+
+    return length;
+  }
+
+  at(index: number): Node {
+    if (!this.head) {
+      throw new Error('Index out of bounds');
+    }
+
+    let counter = 0;
+
+    // remmember use case of Type Annotations: see https://docs.google.com/document/d/14DDmG_a4Ikb3Z7RFkRI0R0sbN7GBObehY6i2JC_1Ygg/edit#heading=h.if8imnf337kd
+    let node: Node | null = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+
+      counter++;
+      node = node.next;
+    }
+
+    throw new Error('Index out of bounds');
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    if (!this.head) {
+      throw new Error('List is empty');
+    }
+
+    return this.at(leftIndex).data > this.at(rightIndex).data;
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftNode = this.at(leftIndex);
+    const rightNode = this.at(rightIndex);
+
+    const leftHand = leftNode.data;
+    leftNode.data = rightNode.data;
+    rightNode.data = leftHand;
+  }
+
+  print(): void {
+    if (!this.head) {
+      return;
+    }
+
+    let node: Node | null = this.head;
+    while (node) {
+      console.log(node.data);
+      node = node.next;
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { CharactersCollections } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
@@ -7,7 +8,17 @@ import { Sorter } from './Sorter';
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollections = new CharactersCollections('Xaayb');
-const sorter = new Sorter(charactersCollections);
+// const charactersCollections = new CharactersCollections('Xaayb');
+// const sorter = new Sorter(charactersCollections);
+// sorter.sort();
+// console.log(charactersCollections.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollections.data);
+linkedList.print();
