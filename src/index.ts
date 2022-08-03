143a5f0a9924bffab33ae42315ca18883b6d9fb4
Joliver Estampador <joliverestampador@gmail.com>
2022-08-04 07:55:06 +0800

    completed sort app

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
 import { CharactersCollections } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 import { NumbersCollection } from './NumbersCollection';
-import { Sorter } from './Sorter';
 
-// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
-// const charactersCollections = new CharactersCollections('Xaayb');
-// const sorter = new Sorter(charactersCollections);
-// sorter.sort();
-// console.log(charactersCollections.data);
+const charactersCollections = new CharactersCollections('Xaayb');
+charactersCollections.sort();
+console.log(charactersCollections.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
@@ -19,6 +16,5 @@ linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
